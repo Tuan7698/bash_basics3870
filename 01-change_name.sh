e594e656bd6c60fec0a664024b310a48617e3c6c
Tuan7698 <tnguyen194@ggc.edu>
2026-01-29 16:24:02 -0500

    Add bash script with user name and school

---
+++ 01-change_name.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello Tuan!"
+echo "How old are you?" 
+read age
+echo 'Thanks Tuan! I see you are ' $age ' years old'
+
+echo "What school do you currently attend?"
+read school
+echo 'Nice! ' GGC ' sounds like a great place to learn.'
